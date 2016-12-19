b13e0dd1130a2f31a5d10a3b20c8447f8f54e348
Dave Parsons <dave@daveparsons.net>
2016-12-19 12:26:16 +0000

    Allow unlocker to be disabled on ESXi via boot option

--- local.sh
+++ local.sh
@@ -6,15 +6,27 @@ echo VMware ESXi 6.x Unlocker 2.0.9
 echo ===============================
 echo Copyright: Dave Parsons 2011-16
 
-/etc/init.d/hostd status
-/etc/init.d/hostd stop
+if bootOption -o | grep -q 'nounlocker'; then
 
-vmkramdisk /bootbank/unlocker.vgz
+    echo Unlocker disabled via boot options >> /var/log/unlocker.log
 
-sed -i 's/applesmc/vmkernel/g' /lib/libvmkctl.so
-if [ -f /lib64/libvmkctl.so ]; then
-    sed -i 's/applesmc/vmkernel/g' /lib64/libvmkctl.so
+else
+
+    echo Unlocker running >> /var/log/unlocker.log
+
+    /etc/init.d/hostd status >> /var/log/unlocker.log
+    /etc/init.d/hostd stop >> /var/log/unlocker.log
+
+    vmkramdisk /bootbank/unlocker.vgz >> /var/log/unlocker.log
+
+    sed -i 's/applesmc/vmkernel/g' /lib/libvmkctl.so
+    if [ -f /lib64/libvmkctl.so ]; then
+        sed -i 's/applesmc/vmkernel/g' /lib64/libvmkctl.so
+    fi
+
+    /etc/init.d/hostd start  >> /var/log/unlocker.log
+    /etc/init.d/hostd status >> /var/log/unlocker.log
+
 fi
 
-/etc/init.d/hostd start
-/etc/init.d/hostd status
+exit 0
