5aa7936fa7e19f51e6ef6d8ddc61c25d8ea3d302
Dave Parsons <dave@daveparsons.net>
2018-10-20 17:29:27 +0100

    Windows commands fixed to work with Player plus cosmetic changes to output.

--- win-test-install.cmd
+++ win-test-install.cmd
@@ -1,22 +1,30 @@
 @echo off
 setlocal ENABLEEXTENSIONS
+echo.
 echo Unlocker 3.0.2 for VMware Workstation
 echo =====================================
 echo (c) Dave Parsons 2011-18
+echo.
+echo Set encoding parameters...
+chcp 850
 
 net session >NUL 2>&1
 if %errorlevel% neq 0 (
-    echo Administrator privileges required! 
-    exit
+    echo Administrator privileges required!
+    exit /b
 )
 
-pushd %~dp0
-
-set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Workstation"
+echo.
+set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Player"
 :: delims is a TAB followed by a space
 for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v InstallPath') do set InstallPath=%%B
 echo VMware is installed at: %InstallPath%
+for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v ProductVersion') do set ProductVersion=%%B
+echo VMware product version: %ProductVersion%
 
+pushd %~dp0
+
+echo.
 echo Stopping VMware services...
 net stop vmware-view-usbd > NUL 2>&1
 net stop VMwareHostd > NUL 2>&1
@@ -24,6 +32,7 @@ net stop VMAuthdService > NUL 2>&1
 net stop VMUSBArbService > NUL 2>&1
 taskkill /F /IM vmware-tray.exe > NUL 2>&1
 
+echo.
 echo Backing up files...
 rd /s /q .\backup > NUL 2>&1
 mkdir .\backup
@@ -33,13 +42,16 @@ xcopy /F /Y "%InstallPath%x64\vmware-vmx-debug.exe" .\backup\x64
 xcopy /F /Y "%InstallPath%x64\vmware-vmx-stats.exe" .\backup\x64
 xcopy /F /Y "%InstallPath%vmwarebase.dll" .\backup\
 
+echo.
 echo Patching...
 python unlocker.py
 
+echo.
 echo Getting VMware Tools...
 python gettools.py
 xcopy /F /Y .\tools\darwin*.* "%InstallPath%"
 
+echo.
 echo Starting VMware services...
 net start VMUSBArbService > NUL 2>&1
 net start VMAuthdService > NUL 2>&1
@@ -47,5 +59,5 @@ net start VMwareHostd > NUL 2>&1
 net start vmware-view-usbd > NUL 2>&1
 
 popd
-
+echo.
 echo Finished!
--- win-uninstall.cmd
+++ win-uninstall.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal ENABLEEXTENSIONS
+echo.
 echo Unlocker 3.0.2 for VMware Workstation
 echo =====================================
 echo (c) Dave Parsons 2011-18
@@ -10,17 +11,17 @@ if %errorlevel% neq 0 (
     exit
 )
 
-pushd %~dp0
-
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
-    set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Workstation"
-) else (
-    set KeyName="HKLM\SOFTWARE\VMware, Inc.\VMware Workstation"
-)
+echo.
+set KeyName="HKLM\SOFTWARE\Wow6432Node\VMware, Inc.\VMware Player"
 :: delims is a TAB followed by a space
 for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v InstallPath') do set InstallPath=%%B
 echo VMware is installed at: %InstallPath%
+for /F "tokens=2* delims=	 " %%A in ('REG QUERY %KeyName% /v ProductVersion') do set ProductVersion=%%B
+echo VMware product version: %ProductVersion%
 
+pushd %~dp0
+
+echo.
 echo Stopping VMware services...
 net stop vmware-view-usbd > NUL 2>&1
 net stop VMwareHostd > NUL 2>&1
@@ -28,15 +29,18 @@ net stop VMAuthdService > NUL 2>&1
 net stop VMUSBArbService > NUL 2>&1
 taskkill /F /IM vmware-tray.exe > NUL 2>&1
 
+echo.
 echo Restoring files...
 xcopy /F /Y .\backup\x64\*.* "%InstallPath%x64\"
 xcopy /F /Y .\backup\*.* "%InstallPath%"
 del /f "%InstallPath%"darwin*.*
 
+echo.
 echo Removing backup files...
 rd /s /q .\backup > NUL 2>&1
 rd /s /q .\tools > NUL 2>&1
 
+echo.
 echo Starting VMware services...
 net start VMUSBArbService > NUL 2>&1
 net start VMAuthdService > NUL 2>&1
@@ -44,5 +48,5 @@ net start VMwareHostd > NUL 2>&1
 net start vmware-view-usbd > NUL 2>&1
 
 popd
-
+echo.
 echo Finished!
