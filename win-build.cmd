5f4c6ccaf148fb363bcd206fd55efa9c26b204ce
BDisp <bd.bdisp@gmail.com>
2019-10-14 14:28:27 +0100

    Recompiled and because I use VS 2019

--- win-build.cmd
+++ win-build.cmd
@@ -2,6 +2,7 @@
 rd /s /q build
 rd /s /q dist
 del *.spec
+rd /s /q __pycache__
 pyinstaller --onefile dumpsmc.py
 pyinstaller --onefile gettools.py
 pyinstaller --onefile unlocker.py
@@ -9,3 +10,4 @@ xcopy /y dist\*.exe .
 rd /s /q build
 rd /s /q dist
 del *.spec
+rd /s /q __pycache__
